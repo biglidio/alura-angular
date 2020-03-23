26dd8f20053c0410b25dcc1f564ed0fdd57ad580
Lucas <lucas-fj@hotmail.com>
2020-03-23 18:25:41 +0100

    Lesson #1 - Angular installation, components and data binding

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # alura-angular
+
+Install Node.js v8.X (Install NVM to switch your Node.js version)
+
+npm install -g @angular/cli@6.0.7 (Need admin privilegies)
+
+ng new alurapic
+
+cd alurapic
+
+npm install rxjs@6.0.0 --save
+
+ng serve --open
---
+++ alurapic/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  url = "https://pmcvariety.files.wordpress.com/2018/11/rev-1-fb2-00589r_high_res_jpeg.jpg?w=1000&h=563&crop=1";
+  description = "Fantastic Beasts";
+}
