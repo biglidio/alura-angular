4363441a35ddc322cbd87e2b509c611b28b7d69f
Lucas <lucas-fj@hotmail.com>
2020-03-23 22:15:13 +0100

    Lesson #3 - Consuming WebAPI, HttpClient + Inject, API Interface and component cicle

--- alurapic/src/app/app.component.ts
+++ alurapic/src/app/app.component.ts
@@ -1,20 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PhotoService } from './photos/photo/photo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  photos = [
-    {
-      url: "https://pmcvariety.files.wordpress.com/2018/11/rev-1-fb2-00589r_high_res_jpeg.jpg?w=1000&h=563&crop=1",
-      description: "Fantastic Beasts"
-    },
-    {
-      url: "https://hermandadblanca.org/wp-content/uploads/2019/09/hipogrifo-hipogrifo-un-ser-mitol%C3%B3gico.-I211579.jpg",
-      description: "Hipogrifo"
-    }
-  ];
+  photos: Object[] = [];
+
+  constructor(private photoService: PhotoService) {}
+  
+  ngOnInit(): void {
+    
+    this.photoService
+      .listFromUser("Flavio")
+      .subscribe(photos => this.photos = photos);
+  }
 }
