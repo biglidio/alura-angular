9231ded5ff21915b023e048db84f6f8ec522709a
Lucas <lucas-fj@hotmail.com>
2020-03-23 21:16:11 +0100

    Lesson #2 - Adding dependencies, importing components, inbound properties, modules and directives

--- alurapic/src/app/app.component.ts
+++ alurapic/src/app/app.component.ts
@@ -6,7 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  url = "https://pmcvariety.files.wordpress.com/2018/11/rev-1-fb2-00589r_high_res_jpeg.jpg?w=1000&h=563&crop=1";
-  description = "Fantastic Beasts";
+
+  photos = [
+    {
+      url: "https://pmcvariety.files.wordpress.com/2018/11/rev-1-fb2-00589r_high_res_jpeg.jpg?w=1000&h=563&crop=1",
+      description: "Fantastic Beasts"
+    },
+    {
+      url: "https://hermandadblanca.org/wp-content/uploads/2019/09/hipogrifo-hipogrifo-un-ser-mitol%C3%B3gico.-I211579.jpg",
+      description: "Hipogrifo"
+    }
+  ];
 }
