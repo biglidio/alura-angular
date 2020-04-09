2631effa86f87b7594ad9dd87322b198b14320aa
Lucas <lucas-fj@hotmail.com>
2020-04-09 20:23:18 +0200

    Lesson #8 - Login, form validation, redirect and platform detection

---
+++ alurapic/src/app/shared/components/vmessage/vmessage.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+import { VMessageComponent } from "./vmessage.component";
+import { CommonModule } from "@angular/common";
+
+@NgModule({
+    declarations: [
+        VMessageComponent
+    ],
+    exports: [
+        VMessageComponent
+    ],
+    imports: [
+        CommonModule
+    ]
+})
+export class VMessageModule {}
