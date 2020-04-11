632e39fcf1db6a12a44c5796079de13cc253d195
Lucas <lucas-fj@hotmail.com>
2020-04-11 20:59:10 +0200

    Lesson #10 - BehaviorSubject, Async pipe, logout, routes guard and routerLink

--- alurapic/src/app/core/core.module.ts
+++ alurapic/src/app/core/core.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from "@angular/core";
 import { HeaderComponent } from "./header/header.component";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
+    imports: [CommonModule, RouterModule],
     declarations: [HeaderComponent],
     exports: [HeaderComponent]
 })
