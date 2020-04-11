5b90415152503c0bfb69263ba5b5a116851d8e20
Lucas <lucas-fj@hotmail.com>
2020-04-12 00:41:05 +0200

    Lesson #11 - Register, form validation, custom validation, async validation and data submission

--- alurapic/src/app/home/home.module.ts
+++ alurapic/src/app/home/home.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from "@angular/core";
 import { SignInComponent } from "./signin/signin.component";
-import { ReactiveFormsModule } from "@angular/forms";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
 import { VMessageModule } from "../shared/components/vmessage/vmessage.module";
+import { SignUpComponent } from "./signup/signup.component";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
     declarations: [
-        SignInComponent
+        SignInComponent,
+        SignUpComponent
     ],
     imports: [
         CommonModule,
+        FormsModule,
         ReactiveFormsModule,
-        VMessageModule
+        VMessageModule,
+        RouterModule
     ]
 })
 export class HomeModule {}
